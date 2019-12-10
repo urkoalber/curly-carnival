79f82bb5d8620568a39bd760bef52cb05e57a2d2
Urko <urkito91@hotmail.com>
2019-12-10 12:29:28 +0000

    initial commit

---
+++ src/components/RemoteConsumer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RemoteConsumer(props) {
+  return (
+    <div className="RemoteConsumer">
+      <p>Consuming {props.url}</p>
+      {/* WRITE YOUR CODE HERE */}
+    </div>
+  );
+}
+
+export default RemoteConsumer;
